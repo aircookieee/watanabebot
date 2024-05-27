18bbee24a583230aff1f5f238a819abfc1fc857b
aircookieee <98703460+aircookieee@users.noreply.github.com>
2024-05-27 20:01:59 +0200

    if it ain't broken don't fix it

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,17 @@
 import Discord = require("discord.js");
 import fs = require("fs");
+import ts = require("typescript");
+import axios from "axios";
+import * as deepl from 'deepl-node';
+import webhookIDs from '../channelsID.json';
 
-const auth = require("./auth.json");
+const auth = require("../auth.json");
 
 let watashiSearch = /(?<![a-zA-Z])You(?!\s*\(not [Ww]atanabe\)|[a-zA-Z])/gm;
 let smolWatashiSearch = /(?<![a-zA-Z])you-chan(?!\s*\(not [Ww]atanabe\)|[a-zA-Z])/gm;
-let yesWatanabeSearch = /\(yes Watanabe\)/gm;
+let yesWatanabeSearch = /\(yes [Ww]atanabe\)/gm;
+let twitterLinkNew = /(https:\/\/((x.com)|(twitter.com))\/(\w+)(\/(\w+)\/(\w+))?)/gi;
+let nitterLinkNew = /(https:\/\/nitter\.net\/(\w+)(\/(\w+)\/(\w+))?)/gi;
 let animeChannelID: {
   [guildID: string]: string;
 };
@@ -14,10 +20,13 @@ let commandSearch = /^!yousoro(?:$| (.+))/;
 let databasePath = "database.json";
 let bot: Discord.Client;
 
+let twitterGlobalToggle: string = String(fs.readFileSync("build/twitter.txt"));
+let pluralKitUID = "466378653216014359";
+
 try {
   let database = fs.readFileSync(databasePath).toString();
   animeChannelID = JSON.parse(database);
-} catch (err) {
+} catch (err: any) {
   if ((err.code = "ENOENT")) {
     let fd = fs.openSync(databasePath, "w+");
     fs.closeSync(fd);
@@ -27,30 +36,16 @@ try {
   }
 }
 
-function watashi(channel: Discord.TextChannel | Discord.DMChannel): void {
+function watashi(channel: Discord.TextChannel | Discord.DMChannel, message: Discord.Message): void {
   if (channel instanceof Discord.TextChannel && !canSend(channel)) return;
-  channel
-    .send("Watashi?", {
-      files: [
-        {
-          attachment: "resources/watashi.jpg",
-        },
-      ],
-    })
-    .catch();
+  message
+    .react("1122673094750896228").catch(err => {channel.send("Nosoro :(")});
 }
 
-function smolWatashi(channel: Discord.TextChannel | Discord.DMChannel): void {
+function smolWatashi(channel: Discord.TextChannel | Discord.DMChannel, message: Discord.Message): void {
   if (channel instanceof Discord.TextChannel && !canSend(channel)) return;
-  channel
-    .send("ʷᵃᵗᵃˢʰᶦˀ", {
-      files: [
-        {
-          attachment: "resources/smolsoro.png",
-        },
-      ],
-    })
-    .catch();
+  message
+    .react("853643607612325888").catch(err => {channel.send("Nosoro :(")});
 }
 
 function yousoroInfo(channel: Discord.TextChannel): void {
@@ -127,17 +122,10 @@ function nosoro(channel: Discord.TextChannel | Discord.DMChannel): void {
     .catch();
 }
 
-function yesWatanabe(channel: Discord.TextChannel | Discord.DMChannel): void {
+function yesWatanabe(channel: Discord.TextChannel | Discord.DMChannel, message: Discord.Message): void {
   if (channel instanceof Discord.TextChannel && !canSend(channel)) return;
-  channel
-    .send({
-      files: [
-        {
-          attachment: "resources/yesWatanabe.png",
-        },
-      ],
-    })
-    .catch();
+  message
+    .react("911047098236010566").catch(err => {channel.send("Nosoro :(")});
 }
 
 function canSend(channel: Discord.TextChannel): boolean {
@@ -152,6 +140,81 @@ function writeDatabase(): void {
   fs.writeFileSync(databasePath, JSON.stringify(animeChannelID));
 }
 
+// function to replace twitter links with vxtwitter (now with more X!)
+function fixTwitterEmbeds(channel: Discord.TextChannel | Discord.DMChannel, message: Discord.Message) {
+  if (message.webhookID && message.application?.id != pluralKitUID) return;  // I hate everyone but pluralkit
+  let vxLink = "";
+  let finalMessage = "";
+  let tmsg = message.content.match(twitterLinkNew)!;  // I fuckign HATE regex
+  if (tmsg == null)
+    tmsg = message.content.match(nitterLinkNew)!; // I fuckign HATE regex
+  for (let i = 0; i < tmsg.length; i++) {
+    if (tmsg[i].includes("vxtwitter") || tmsg[i].includes("fxtwitter")) continue;
+    if (tmsg[i].includes("https://x.com/"))
+      vxLink = tmsg[i].replace("x", "vxtwitter");
+    else if (tmsg[i].includes("https://twitter.com/"))
+      vxLink = tmsg[i].replace("twitter", "vxtwitter");
+    else if (tmsg[i].includes("https://nitter.net/"))
+      vxLink = tmsg[i].replace("nitter.net", "vxtwitter.com");
+    if (finalMessage.includes(vxLink)) continue;
+    finalMessage = finalMessage.concat(vxLink + "\n");
+  }
+  channel.send(finalMessage).catch(err => console.log("Exception occurred! " + err));
+}
+
+/* function buildAnimeThemesEmbed(animeThemesGeneralData: any, themeType: string) {
+  console.log("https://v.animethemes.moe/" + animeThemesGeneralData.anime.slug + "-" + themeType + ".webm")
+  const animeThemesEmbed = new Discord.MessageEmbed()
+    .setColor(0x0099FF)
+    .setTitle(String(animeThemesGeneralData.anime.name))
+    .setURL("https://animethemes.moe/anime/" + animeThemesGeneralData.anime.slug)
+    .setAuthor({name: "You-chan's Magic Cannon", iconURL: "https://files.catbox.moe/jr6tkw.jpg"})
+    .setVideo = ("https://v.animethemes.moe/" + animeThemesGeneralData.anime.slug + "-" + themeType + ".webm")
+  return animeThemesEmbed;
+} 
+
+async function sendAnimeThemesWebhook(channel: Discord.TextChannel) {
+  let webhook = await channel.createWebhook("You-chan's Magic Cannon", {avatar: "https://files.catbox.moe/jr6tkw.jpg"})
+  await webhook.send(embed);
+  webhook.delete();
+} */
+
+async function animeThemesVideo(animeString: string) {
+  try {
+    let { data } = await axios.get(
+      "https://api.animethemes.moe/video/?q=" + animeString
+    )
+    return data;
+  } catch(err){
+    console.log(err);
+  }
+}
+
+async function callAnimeThemes(arg: string, channel: Discord.TextChannel){
+  const animeName = arg.slice(6);
+  const animeThemesVideoData = await animeThemesVideo(animeName)
+  try {
+  channel.send("["+ animeThemesVideoData.videos['0'].filename + " (" + animeThemesVideoData.videos['0'].source +")]("+animeThemesVideoData.videos['0'].link+")");
+  } catch(err){channel.send("No themes found, either the animethemes search sucks or you misspelled something.")}
+}
+
+async function requestTranslationData(messageEmbed: Discord.MessageEmbed): Promise<string> {
+  const translator = new deepl.Translator(auth.deeplAuthKey);
+  console.log("Translator created successfully");
+  if (messageEmbed.description == null) {
+    console.log("Embed description empty!");
+    return "";
+  }
+  let tlTextResult = await translator.translateText(messageEmbed.description, 'ja', 'en-US'); // let's hope this never times out
+  return tlTextResult.text;
+}
+
+async function translateEmbedData(message: Discord.Message) {
+  let deeplOutput = await requestTranslationData(message.embeds[0]);
+  console.log("Got translation data: " + deeplOutput);
+  message.channel.send(deeplOutput);
+}
+
 // Initialize Discord Bot
 bot = new Discord.Client();
 
@@ -161,6 +224,8 @@ bot.on("ready", () => {
   console.log("Connected");
   console.log("Logged in as: ");
   console.log(bot.user?.username + " - (" + bot.user?.id + ")");
+  console.log("You-chan started at " + new Date());
+  // console.log("DeepL Auth Key: " + auth.deeplAuthKey);
   mentionSearch = new RegExp("<@!?" + bot.user?.id + ">", "gm");
   for (const [id, guild] of bot.guilds.cache) {
     if (!animeChannelID[id]) {
@@ -185,8 +250,9 @@ bot.on("message", (message) => {
   let command = content.match(commandSearch);
   let channel: Discord.TextChannel | Discord.DMChannel | undefined;
 
-  // Check if user is bot, skip if it is
-  if (message.author.bot) return;
+  // Check if user is bot and not a webhook, skip if both conditions are met
+  if (message.author.bot && !message.webhookID) 
+    return;
 
   if (
     message.channel instanceof Discord.TextChannel ||
@@ -195,11 +261,24 @@ bot.on("message", (message) => {
     channel = message.channel;
   else return;
 
+  // call deepl translation API on current message
+  /* if (message.embeds[0] != null) 
+    console.log(new Date() + " Embed found, expected " + webhookIDs.musicartWebhookID + ", got author ID " + message.author.id + " webhook ID " + message.webhookID); */
+  if (message.author.id == webhookIDs.musicartWebhookID && message.embeds[0].description != "") {
+    console.log("Called function on message " + message.id)
+    translateEmbedData(message);
+  }
+
+  if (twitterGlobalToggle == "true")
+    if (content.includes("https://x.com/") || content.includes("https://twitter.com/") || content.includes("https://nitter.net/")) 
+      fixTwitterEmbeds(channel, message);
   if (command) {
     if (channel instanceof Discord.DMChannel) {
       yousoroDMs(channel);
     } else if (command[0] == "!yousoro") {
       yousoroInfo(channel);
+    } else if (command[1].slice(0, 5) == "theme") {
+      callAnimeThemes(command[1], channel);
     } else if (
       message.member?.hasPermission("ADMINISTRATOR") &&
       command.length >= 2
@@ -209,8 +288,14 @@ bot.on("message", (message) => {
         yousoroHere(channel);
       } else if (arg == "everywhere") {
         yousoroEverywhere(channel);
-      } else {
-        nosoro(channel);
+      } else if (arg == "twitter on") {
+          twitterGlobalToggle = "true";
+          channel.send("Twitter Link Replacement is **ON**");
+          fs.writeFileSync('build/twitter.txt', String(twitterGlobalToggle));
+      } else if (arg == "twitter off") {
+          twitterGlobalToggle = "false";
+          channel.send("Twitter Link Replacement is **OFF**");
+          fs.writeFileSync('build/twitter.txt', String(twitterGlobalToggle));
       }
     } else {
       nosoro(channel);
@@ -222,15 +307,15 @@ bot.on("message", (message) => {
       animeChannelID[message.guild.id] == message.channel.id
     ) {
       if (content.search(watashiSearch) > -1) {
-        if (content.search(yesWatanabeSearch) > -1) yesWatanabe(channel);
-        else watashi(channel);
+        if (content.search(yesWatanabeSearch) > -1) yesWatanabe(channel, message);
+        else watashi(channel, message);
       } else if (content.search(smolWatashiSearch) > -1) {
-        smolWatashi(channel);
+        smolWatashi(channel, message);
       }
     }
 
     if (content.search(mentionSearch) > -1) {
-      watashi(channel);
+      watashi(channel, message);
     }
   }
 });
